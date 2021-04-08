d40b7bbca9ec645265857dc6c6e3f747c49efba2
Аня <ganna.danilenka@gmail.com>
2021-04-08 16:50:06 +0300

    initial

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {NavLink, useHistory} from 'react-router-dom'
+import {AuthContext} from '../context/AuthContext'
+import {useContext} from 'react'
+
+
+export const Navbar = () => {
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+    const logoutHandler = event => {
+        event.preventDefault()
+        auth.logout()
+        history.push('/')
+    }
+    return (
+        <nav>
+            <div className="nav-wrapper indigo darken-1" style={{ padding: '0 2rem'}}>
+                <span href="/" className="brand-logo">Links shortcut</span>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><NavLink to="/create">Create</NavLink></li>
+                    <li><NavLink to="/links">Links</NavLink></li>
+                    <li><a href="/" onClick={logoutHandler}>Logout</a></li>
+
+                </ul>
+            </div>
+        </nav>
+    )
+}
